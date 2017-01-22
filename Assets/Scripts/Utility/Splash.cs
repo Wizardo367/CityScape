777d579609060480a98ed4676b64527cab3d839a
Harish Bhagat <h.bhagat@hotmail.co.uk>
2017-01-22 17:23:22 +0000

    Splash Screen Added
    A splash screen was added and the office building prefab was also
    changed.

---
+++ Assets/Scripts/Utility/Splash.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// Website used: http://pixlab-games.tumblr.com/post/121686571742/how-to-make-a-custom-splash-screen-in-unity-5
+
+public class Splash : MonoBehaviour
+{
+    private float _timer;
+    public float Duration = 3f;
+
+    void Start()
+    {
+        _timer = 0;
+    }
+
+    void Update()
+    {
+        _timer += Time.deltaTime;
+        if (_timer >= Duration)
+            SceneManager.LoadScene("Game");
+    }
+}
