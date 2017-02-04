a947855840bbf04b46770cee4198f8808e5fd912
Harish Bhagat <h.bhagat@hotmail.co.uk>
2017-02-04 19:35:35 +0000

    Markers Now Spawn Buildings
    Markers now turn into buldings after a set delay.

---
+++ Assets/Scripts/Game/Marker.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class Marker : MonoBehaviour
+{
+    public TileType Type;
+    public GameObject Target;
+    [Tooltip("Delay till the target is shown, in seconds.")]
+    public float Delay = 5;
+
+    private Time _startTime;
+    private float _timeRemaining;
+
+    private void Start()
+    {
+        _timeRemaining = Delay;
+    }
+
+    private void Update()
+    {
+        // Countdown then display target
+        _timeRemaining -= Time.deltaTime;
+
+        if (_timeRemaining <= 0)
+        {
+            // Get map
+            Map2D map = GameObject.Find("Game").GetComponent<Map2D>();
+
+            // Get building type (Assumes Type is set correctly)
+            string typeString = Type.ToString();
+            TileType buildingType = (TileType)System.Enum.Parse(typeof(TileType), typeString.Substring(0, typeString.Length - 6));
+
+            // Spawn building
+            map.SpawnBuilding(buildingType, 1, 1, gameObject.transform.position);
+
+            // Remove marker
+            Destroy(gameObject);
+        }
+    }
+}
